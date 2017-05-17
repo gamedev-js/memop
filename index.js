1b5b977ed6b04a863d786da4fa040170e8d8b492
Johnny Wu <KarasAya@gmail.com>
2017-05-17 17:15:14 +0800

    add recycle-pool

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export { default as FixedArray } from './lib/fixed-array';
 export { default as Pool } from './lib/pool';
 export { default as FramePool } from './lib/frame-pool';
+export { default as RecyclePool } from './lib/recycle-pool';
---
+++ test/recycle-pool.spec.js
@@ -0,0 +1,89 @@
+const tap = require('tap');
+const { RecyclePool } = require('../dist/memop');
+
+tap.test('RecyclePool', t => {
+  t.test('add', t => {
+    let arr = new RecyclePool(function () {
+      return {
+        id: -1,
+        age: 0,
+        velocity: [1, 1, 1],
+      };
+    }, 100);
+
+    arr.add().id = 10;
+    arr.add().id = 20;
+    arr.add().id = 30;
+    arr.add().id = 40;
+    arr.add().id = 50;
+
+    t.equal(arr.length, 5);
+    t.equal(arr.data.length, 100);
+    t.equal(arr.data[0].id, 10);
+    t.equal(arr.data[1].id, 20);
+    t.equal(arr.data[2].id, 30);
+    t.equal(arr.data[3].id, 40);
+    t.equal(arr.data[4].id, 50);
+
+    t.end();
+  });
+
+  t.test('remove', t => {
+    let arr = new RecyclePool(function () {
+      return {
+        id: -1,
+        age: 0,
+        velocity: [1, 1, 1],
+      };
+    }, 100);
+
+    arr.add().id = 10;
+    arr.add().id = 20;
+    arr.add().id = 30;
+    arr.add().id = 40;
+    arr.add().id = 50;
+
+    arr.remove(2);
+
+    t.equal(arr.length, 4);
+    t.equal(arr.data.length, 100);
+    t.equal(arr.data[0].id, 10);
+    t.equal(arr.data[1].id, 20);
+    t.equal(arr.data[2].id, 50);
+    t.equal(arr.data[3].id, 40);
+
+    t.equal(arr.data[4].id, 30);
+
+    t.end();
+  });
+
+  t.test('reset', t => {
+    let arr = new RecyclePool(function () {
+      return {
+        id: -1,
+        age: 0,
+        velocity: [1, 1, 1],
+      };
+    }, 100);
+
+    arr.add().id = 10;
+    arr.add().id = 20;
+    arr.add().id = 30;
+    arr.add().id = 40;
+    arr.add().id = 50;
+
+    arr.reset(0);
+
+    t.equal(arr.length, 0);
+    t.equal(arr.data.length, 100);
+    t.equal(arr.data[0].id, 10);
+    t.equal(arr.data[1].id, 20);
+    t.equal(arr.data[2].id, 30);
+    t.equal(arr.data[3].id, 40);
+    t.equal(arr.data[4].id, 50);
+
+    t.end();
+  });
+
+  t.end();
+});
