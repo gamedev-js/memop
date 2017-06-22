bdecfb413f902f757992f79b5f8a25c11e204eb6
Johnny Wu <KarasAya@gmail.com>
2017-06-22 16:16:41 +0800

    bump to 1.4.0

--- dist/memop.dev.js
+++ dist/memop.dev.js
@@ -1,12 +1,102 @@
 
 /*
- * memop v1.3.2
+ * memop v1.4.0
  * (c) 2017 @Johnny Wu
  * Released under the MIT License.
  */
 
 (function (exports) {
 'use strict';
+
+function _compare(a, b) {
+  return a - b;
+}
+
+/**
+ * _swap the places of two elements
+ *
+ * @private
+ * @param {array} array The array which contains the elements
+ * @param {number} i The index of the first element
+ * @param {number} j The index of the second element
+ * @returns {array} array The array with swaped elements
+ */
+function _swap(array, i, j) {
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+
+  return array;
+}
+
+/**
+ * Partitions given subarray using Lomuto's partitioning algorithm.
+ *
+ * @private
+ * @param {array} array Input array
+ * @param {number} left The start of the subarray
+ * @param {number} right The end of the subarray
+ */
+function _partition(array, left, right, cmp) {
+  let cmpVal = array[right - 1];
+  let minEnd = left;
+  let maxEnd;
+
+  for (maxEnd = left; maxEnd < right - 1; maxEnd += 1) {
+    if (cmp(array[maxEnd], cmpVal) < 0) {
+      _swap(array, maxEnd, minEnd);
+      minEnd += 1;
+    }
+  }
+  _swap(array, minEnd, right - 1);
+
+  return minEnd;
+}
+
+/**
+ * Sorts given array.
+ *
+ * @private
+ * @param {array} array Array which should be sorted
+ * @param {number} left The start of the subarray which should be handled
+ * @param {number} right The end of the subarray which should be handled
+ * @returns {array} array Sorted array
+ */
+function _quickSort(array, left, right, cmp) {
+  if (left < right) {
+    let p = _partition(array, left, right, cmp);
+    _quickSort(array, left, p, cmp);
+    _quickSort(array, p + 1, right, cmp);
+  }
+
+  return array;
+}
+
+/**
+ * Calls the _quickSort function with it's initial values.
+ *
+ * @public
+ * @param {array} array The input array which should be sorted
+ * @param {Number} from
+ * @param {Number} to
+ * @param {Function} cmp
+ * @returns {array} array Sorted array
+ */
+var quickSort = function (array, from, to, cmp) {
+  if (from === undefined) {
+    from = 0;
+  }
+
+  if (to === undefined) {
+    to = array.length;
+  }
+
+  if (cmp === undefined) {
+    cmp = _compare;
+  }
+
+  return _quickSort(array, from, to, cmp);
+};
 
 class FixedArray {
   constructor(size) {
@@ -78,6 +168,10 @@ class FixedArray {
     }
 
     return idx;
+  }
+
+  sort(cmp) {
+    return quickSort(this._data, 0, this._count, cmp);
   }
 }
 
@@ -210,6 +304,10 @@ class RecyclePool {
     this._data[idx] = this._data[last];
     this._data[last] = tmp;
     this._count -= 1;
+  }
+
+  sort(cmp) {
+    return quickSort(this._data, 0, this._count, cmp);
   }
 }
 
--- dist/memop.js
+++ dist/memop.js
@@ -1,6 +1,6 @@
 
 /*
- * memop v1.3.2
+ * memop v1.4.0
  * (c) 2017 @Johnny Wu
  * Released under the MIT License.
  */
@@ -8,6 +8,96 @@
 'use strict';
 
 Object.defineProperty(exports, '__esModule', { value: true });
+
+function _compare(a, b) {
+  return a - b;
+}
+
+/**
+ * _swap the places of two elements
+ *
+ * @private
+ * @param {array} array The array which contains the elements
+ * @param {number} i The index of the first element
+ * @param {number} j The index of the second element
+ * @returns {array} array The array with swaped elements
+ */
+function _swap(array, i, j) {
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+
+  return array;
+}
+
+/**
+ * Partitions given subarray using Lomuto's partitioning algorithm.
+ *
+ * @private
+ * @param {array} array Input array
+ * @param {number} left The start of the subarray
+ * @param {number} right The end of the subarray
+ */
+function _partition(array, left, right, cmp) {
+  let cmpVal = array[right - 1];
+  let minEnd = left;
+  let maxEnd;
+
+  for (maxEnd = left; maxEnd < right - 1; maxEnd += 1) {
+    if (cmp(array[maxEnd], cmpVal) < 0) {
+      _swap(array, maxEnd, minEnd);
+      minEnd += 1;
+    }
+  }
+  _swap(array, minEnd, right - 1);
+
+  return minEnd;
+}
+
+/**
+ * Sorts given array.
+ *
+ * @private
+ * @param {array} array Array which should be sorted
+ * @param {number} left The start of the subarray which should be handled
+ * @param {number} right The end of the subarray which should be handled
+ * @returns {array} array Sorted array
+ */
+function _quickSort(array, left, right, cmp) {
+  if (left < right) {
+    let p = _partition(array, left, right, cmp);
+    _quickSort(array, left, p, cmp);
+    _quickSort(array, p + 1, right, cmp);
+  }
+
+  return array;
+}
+
+/**
+ * Calls the _quickSort function with it's initial values.
+ *
+ * @public
+ * @param {array} array The input array which should be sorted
+ * @param {Number} from
+ * @param {Number} to
+ * @param {Function} cmp
+ * @returns {array} array Sorted array
+ */
+var quickSort = function (array, from, to, cmp) {
+  if (from === undefined) {
+    from = 0;
+  }
+
+  if (to === undefined) {
+    to = array.length;
+  }
+
+  if (cmp === undefined) {
+    cmp = _compare;
+  }
+
+  return _quickSort(array, from, to, cmp);
+};
 
 class FixedArray {
   constructor(size) {
@@ -79,6 +169,10 @@ class FixedArray {
     }
 
     return idx;
+  }
+
+  sort(cmp) {
+    return quickSort(this._data, 0, this._count, cmp);
   }
 }
 
@@ -211,6 +305,10 @@ class RecyclePool {
     this._data[idx] = this._data[last];
     this._data[last] = tmp;
     this._count -= 1;
+  }
+
+  sort(cmp) {
+    return quickSort(this._data, 0, this._count, cmp);
   }
 }
 
