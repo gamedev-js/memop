e6b3cf3951113017e78072582e1f3e7508da47fa
Johnny Wu <karasaya@gmail.com>
2018-01-20 16:26:09 +0800

    update rollup

--- script/rollup.config.js
+++ script/rollup.config.js
@@ -14,31 +14,43 @@ let banner = `
 
 let dest = './dist';
 let file = 'memop';
-let moduleName = 'memop';
+let name = 'memop';
+let sourcemap = true;
+let globals = {
+  'vmath': 'window.vmath',
+  'scene-graph': 'window.sgraph',
+  'WebGLRenderingContext': 'window.WebGLRenderingContext',
+};
 
 // clear directory
 fsJetpack.dir(dest, { empty: true });
 
 module.exports = {
-  entry: './index.js',
-  targets: [
-    { dest: `${dest}/${file}.dev.js`, format: 'iife' },
-    { dest: `${dest}/${file}.js`, format: 'cjs' },
-  ],
-  moduleName,
-  banner,
+  input: './index.js',
   external: [
     'vmath',
     'scene-graph',
     'WebGLRenderingContext',
   ],
-  globals: {
-    'vmath': 'window.vmath',
-    'scene-graph': 'window.sgraph',
-    'WebGLRenderingContext': 'window.WebGLRenderingContext',
-  },
-  sourceMap: true,
   plugins: [
     buble(),
-  ]
+  ],
+  output: [
+    {
+      file: `${dest}/${file}.dev.js`,
+      format: 'iife',
+      name,
+      banner,
+      globals,
+      sourcemap,
+    },
+    {
+      file: `${dest}/${file}.js`,
+      format: 'cjs',
+      name,
+      banner,
+      globals,
+      sourcemap,
+    },
+  ],
 };
